33723a01b7eb1434dcbf44e003a44013579452c7
Divyansh Savita <savitadivyansh323@gmail.com>
2025-08-21 23:45:14 +0530

    h1 tag

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,30 @@ import './App.css'
 function App() {
   const [text,setText]=useState("")
   const [items,setItems]=useState([])
+
   function handleAdd(){
     if(text.trim()==="")return ;
-    setItems([...items,text]);
+    let newItem={
+      id:Date.now(),
+      text:text,
+    }
+    setItems([...items,newItem]);
     setText("")
   }
   
+  function handleDelete(idToRemove){
+    setItems(items.filter(item=>item.id !== idToRemove))
+  }
   return (
     <>
-    <h1>React Notes App</h1>
+    <h1>Trello</h1>
     <input type="text" value={text} onChange={(e)=>setText(e.target.value)} />
     <button onClick={handleAdd}>Click me</button>
     <ul>
-      {items.map((item,index)=>(
-        <li key={index}>{item}</li>
+      {items.map((item)=>(
+        <li key={item.id}>{item.text}{"    "}
+        <button onClick={()=>handleDelete(item.id)}>Delete</button>
+        </li>
       ))}
     </ul>
     </>
